0eb713c58d856b9ec1bc6dbcc5bafb40da3b7ebe
Diego Martin <martind@vitessepsp.com>
2024-06-25 12:57:46 +0200

    Renamed http model property

--- src/WebApiOne/Models.cs
+++ src/WebApiOne/Models.cs
@@ -5,7 +5,7 @@ public static class Models
     public record Member
     {
         public string Email { get; init; } = string.Empty;
-        public string Name { get; init; } = string.Empty;
+        public string FirstName { get; init; } = string.Empty;
         public string LastName { get; init; } = string.Empty;
         public bool IsDeveloper { get; init; }
     }
