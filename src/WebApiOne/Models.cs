b6e4597a8bc566e6419bfe15b755b7208ca70cf0
Diego Martin <martind@vitessepsp.com>
2024-06-25 14:42:20 +0200

    Added property to Member, change OpenApi

--- src/WebApiOne/Models.cs
+++ src/WebApiOne/Models.cs
@@ -7,6 +7,7 @@ public static class Models
         public string Email { get; init; } = string.Empty;
         public string FirstName { get; init; } = string.Empty;
         public string LastName { get; init; } = string.Empty;
+        public Guid? DepartmentId { get; init; }
         public bool IsDeveloper { get; init; }
     }
 
